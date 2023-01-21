a00f48f8b23ad7bd0723cd6f12ceb183c5660f8a
Oneonjr <oneonjunior@outlook.com>
2023-01-20 21:54:11 -0300

    Atualizando Banner

--- ProjetoP/JS/script.js
+++ ProjetoP/JS/script.js
@@ -1,3 +1,12 @@
 $('.popover-dismiss').popover({
     trigger: 'focus'
   })
+
+  function scrolll() {
+    var left = document.querySelector(".scroll-img")
+    left.scrollBy(100, 0)
+  }
+  function scrollr() {
+    var right = document.querySelector(".scroll-img")
+    right.scrollBy(-100, 0)
+  }
